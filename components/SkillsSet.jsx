3fd7dc5fa9aba3c23e43cac7c826192e9d1c2db7
polthemarketer <psocarde@gmail.com>
2022-06-04 23:03:18 +0300

    Update

--- components/SkillsSet.jsx
+++ components/SkillsSet.jsx
@@ -7,7 +7,7 @@ const stats = [
   export default function Example() {
     return (
       <div>
-        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3 mb-20">
           {stats.map((item) => (
             <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
               <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
