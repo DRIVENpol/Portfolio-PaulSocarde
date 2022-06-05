66b1e926dbaf5a185ee4226c5463a98e7237e0ad
polthemarketer <psocarde@gmail.com>
2022-06-05 11:19:59 +0300

    Update Footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "49273bb915598df8"; }
+/******/ 		__webpack_require__.h = function() { return "0ea2cb29a931a866"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
