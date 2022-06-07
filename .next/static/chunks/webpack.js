b5f85d972b18529a6bd609b59958bd8822b923f1
polthemarketer <psocarde@gmail.com>
2022-06-08 00:47:12 +0300

    Dapp Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd0e3a1149e18bd4"; }
+/******/ 		__webpack_require__.h = function() { return "d5c72358b581f974"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
