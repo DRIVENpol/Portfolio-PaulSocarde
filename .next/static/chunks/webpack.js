af987b5e3fe61c3317054b5ecd64aa07464e621a
polthemarketer <psocarde@gmail.com>
2022-06-08 02:15:55 +0300

    static/bg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fa6a927599cd1a2"; }
+/******/ 		__webpack_require__.h = function() { return "5dfe8a75b343e1ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
