1eb2c8076fe5c3d11f40fc684737af60837da56d
polthemarketer <psocarde@gmail.com>
2022-06-11 01:27:47 +0300

    Final ERC20

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f48bca51826d1bd"; }
+/******/ 		__webpack_require__.h = function() { return "a89092b4fb9ea3ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/dapps/CreateERC20.jsx
+++ components/dapps/CreateERC20.jsx
@@ -1,74 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import { CheckCircleIcon } from '@heroicons/react/solid'
 import { XCircleIcon } from '@heroicons/react/solid'
 
 
 const CreateERC20 = (props) => {
+
+    // Token details
     const [tokenName, setTokenName] = useState('');
     const [tokenSymbol, setTokenSymbol] = useState('');
     const [tokenSupply, setTokenSupply] = useState();
 
+
+    // OnChange Handlers
     const nameChangeHandler = (event) => {
         setTokenName(event.target.value);
+        setProps();
     }
 
     const symbolChangeHandler = (event) => {
         setTokenSymbol(event.target.value);
+        setProps();
     }
 
     const supplyChangeHandler = (event) => {
         setTokenSupply(event.target.value);
+        setProps();
     }
 
-
-    props.pd(tokenName, tokenSymbol, tokenSupply);
-
-
-    
-
+    // Transfer props to parent
+    const setProps = () => {
+        props.pd(tokenName, tokenSymbol, tokenSupply);
+    }
 
   return (
-   <><div className="bg-slate-400 dark:bg-slate-700 px-20 py-20">
-
+   <>
+   <div className="bg-slate-400 dark:bg-slate-700 px-20 py-20">
     <form>
     <div className="grid gap-6 lg:grid-cols-2">
      <div>
         <p className="text-3xl text-white font-poppins pt-5 font-semibold">Create a simple ERC20 Token</p>
      </div>
-     
      <div></div>
-     
      <div>
-            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-white">Token Name</label>
+        <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-white">Token Name</label>
             <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 
             text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
             w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Shiba Inu" required 
             onChange={nameChangeHandler}
             />
-        </div>
-        <div>
-            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-white">Token Symbol</label>
+    </div>
+
+    <div>
+        <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-white">Token Symbol</label>
             <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 
             text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
             block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$SINU" required 
                 onChange={symbolChangeHandler}
             />
-        </div>
+    </div>
 
-        <div>
-            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-white">Initial Supply</label>
+     <div>
+        <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-white">Initial Supply</label>
             <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 
             text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
             w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1000" required 
                 onChange={supplyChangeHandler}
             />
-        </div>
+    </div>
 
-<div></div>
+    <div></div>
+
     <div>
        {props.ac ? ( <button
                     type="button"
@@ -95,7 +100,8 @@ const CreateERC20 = (props) => {
     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
     </svg>
     Please connect your wallet...
-</button>)}</div>
+                </button>)}
+    </div>
 
 <div></div>
 
@@ -132,7 +138,6 @@ const CreateERC20 = (props) => {
               <button
                 type="button"
                 className="bg-green-50 px-2 py-1.5 rounded-md text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
-                
               >
                 View transaction
               </button></a>
@@ -144,11 +149,8 @@ const CreateERC20 = (props) => {
     </>
 ): null}
 </div>)}
-
-
         </div>
         </form>
-
 </div>
    </>
   )
